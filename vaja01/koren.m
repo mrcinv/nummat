053bd8baa2cccc7857c88b21f15987a02e267394
Martin Vuk <martin.vuk@fri.uni-lj.si>
2016-02-25 02:27:19 +0100

    Popravil ustavitveni pogoj.

--- vaja01/koren.m
+++ vaja01/koren.m
@@ -3,30 +3,33 @@
 % izračuna kvadratni koren števila x z različnimi metodami
 %
 function y = koren(x,nat=1e-10)
-  y = x/2;
-  while abs(y.^2-x)>nat
-    y = (y+x./y)/2;
+  y = x;
+  for i = 1:100
+    delta = (y-x./y)/2;
+    y = y - delta;
+    %y = (y+x./y)/2;
+    if (abs(y.^2-x)<nat) || (abs(delta)<nat)
+      break
+    end
   end
 endfunction
 
 % Enotski testi
-%!test koren(4)==2
-%!assert (koren(8), 2*koren(2))
+%!test abs(koren(4)-2)<1e-10
+%!assert (koren(8), 2*koren(2), 1e-10)
 %!test
-%! x = rand();
-%! assert(koren(x)^2,x,1e-10)
+%! x = rand(10,1);
+%! assert(koren(x).^2,x,1e-10)
 
 % graf napake
 %!demo
 %! x = linspace(0,5);
-%! for i=1:length(x)
-%!  y(i) = koren(x(i));
-%! end
-%! plot(x,log10(abs(y.^2-x)))
-%! title("Graf napake")
+%! y = koren(x);
+%! plot(x,log10(abs(y.^2-x)),'r',label="$\log_{10}(y^2-x)$")
+%! hold on
+%! plot(x,log10(sqrt(x)-y,'b')
+%! hold off
+%! title("Graf ostanka y^2-x in napake y-sqrt(x)")
 %! %-------------------------
 %! % Graf napake na intervalu [0,5]
 
-% graf časovne zahtevnosti
-%!demo
-%! speed("koren(2,1/n)","",[1,2^15])
